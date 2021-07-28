28000358bac058c779b72bfbeac5084288d53e1d
ParmeshvarModi <2016398@brunel.ac.uk>
2021-07-28 23:41:18 +0530

    controllers and models created

---
+++ recyclebank/src/main/java/com/java/recyclebank/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.java.recyclebank.repositories;
+
+import com.java.recyclebank.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+}
